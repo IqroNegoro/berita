ccc617a824f2dde315ea9595c84fa79e90d2f691
IqroNegoro <iqronegoro0@gmail.com>
2022-01-27 21:19:20 +0700

    .

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -4,9 +4,10 @@ class Home extends Controller {
     public function index() {
         $data["style"] = "assets/css/home/style.css";
         $data["title"] = "Okelah bang";
+        $data["berita"] = $this->model("Home_model")->getBerita();
         $this->view("Templates/header", $data);
         $this->view("Templates/nav");
-        $this->view("home/home");
+        $this->view("home/home", $data);
         $this->view("Templates/footer");
     }
 }
